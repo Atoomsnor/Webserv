e5fcce0cebe6a7b909357e08dc8bf55afc9c4277
Atoomsnor <verschoor.rowan@gmail.com>
2026-05-18 13:51:57 +0200

    wheyy the parsey parse

--- src/parser.cpp
+++ src/parser.cpp
@@ -6,17 +6,46 @@
 
 std::vector<std::string>	Parser::tokenize(const std::string& filepath)
 {
+	//fileparse
 	std::unique_ptr<Logger>& lp = Logger::getInstance();
 	std::ifstream file(filepath);
+	std::vector<std::string> tokens;
 	std::string line;
-	std::vector<std::string> ret;
+	std::string cur;
+
+	
 	while (std::getline(file, line))
 	{
-		lp->printLog("line: {}", line);
-		ret.push_back(line + '\n');
+		for (size_t i = 0; i < line.size(); i++)
+		{
+			char c = line[i];
+			if (c == '{' || c == '}' || c == ';')
+			{
+				if (!cur.empty())
+				{
+					tokens.push_back(cur);
+					cur.clear();
+				}
+				tokens.push_back(std::string(1, c));
+				lp->printLog("line: {}", tokens);
+			}
+			else if (std::isspace(c))
+			{
+				if (!cur.empty())
+				{
+					tokens.push_back(cur);
+					cur.clear();
+				}
+			}
+			else
+			cur += c;
+		}
 	}
-	return (ret);
+	if (!cur.empty())
+		tokens.push_back(cur);
+	return (tokens);
 }
+
 
 
 
