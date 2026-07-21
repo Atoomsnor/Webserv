f7beb346ca269065b3b965b413a724b62e1cabc8
Nils Hendriks <nhendrik@student.codam.nl>
2026-07-21 17:09:11 +0200

    error page makes pages on his own, frankenstein lives

--- src/server/Error.cpp
+++ src/server/Error.cpp
@@ -17,14 +17,19 @@ void	Server::sendError(int fd, int error_code)
 {
 	std::string filepath = "." + getConf(fd).error_pages[error_code];
 	std::ifstream fs(filepath, std::ios::binary);
+	std::string str;
 	if (!fs)
 	{
 		Logger::printLog("Failed to open file {}", filepath);
-		return ;
+		str = HTTP::getResponseCode(error_code);
 	}
-	std::stringstream ss_buffer;
-	ss_buffer << fs.rdbuf();
-	std::string str = ss_buffer.str();
+	else
+	{
+		std::stringstream ss_buffer;
+		ss_buffer << fs.rdbuf();
+		fs.close();
+		str = ss_buffer.str();
+	}
 	std::string response = HTTP::buildResponse(str.size(), str, HTTP::getResponseCode(error_code), getContentType(filepath));
 	Logger::printLog("response: {}", response);
 	sendResponse(fd, response);
